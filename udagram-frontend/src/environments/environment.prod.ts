2235a093811baacef962559c7c17fe2e62ec096c
MubarekHussen <mubahussen2014@gmail.com>
2022-11-03 15:15:36 +0300

    finalizing

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://af096d27018b4498c85c26df27d7ea64-983834287.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
